671990f340f4f4d116f7b0efd2767d3ec7279498
Angel Silva <asilvajr@asilvajr-vbox.(none)>
2014-03-24 19:35:19 -0500

    Added string search, added list merge. currently ineffecient

--- basic_func.py
+++ basic_func.py
@@ -1,26 +1,24 @@
 #!/usr/bin/python
 import sys
 # python basic_func.py < WordGrid.txt
-orig_grid = []
-vert_grid = []
-diag_left = []
-diag_right = []
+WordList=['ALCOHOL','BEER','BYE','COMPUTERS','COOKIES','ELECTTRONIC',
+	'FUCK','GOODGAME','HELLO','HIPPIE','HIPSTER','LOVE','SEX','TELE']
 
 def read_grid(r,grid) :
 	print "inCreate"
-	global orig_grid
+	orig_grid = []
 	line = r.readline()
 	while line != "" :
 		orig_grid.append(line.split())
 		line = r.readline()
-
+	return orig_grid
 def alter_vert(grid):
 	print "inAlterVert"
 	return zip(*grid) #alters the view for it to be vertical
 	#return zip (*grid[::-1]) 
 
 
-def alterDiagLeft(grid) :
+def alter_left(grid) :
 	row_size = len(grid[0])-1
 	col_size = len(grid)-1
 	diag = []
@@ -46,12 +44,11 @@ def alter_right(grid):
         row_size = len(grid[0])-1
         col_size = len(grid)-1
 	diag = []
-        print "Alter right",row_size,col_size
 	# row_size + col_size - 1 => num_of_rows
         for i in xrange(col_size,0,-1) :
                 lst=[]
 		for j in xrange(col_size,i-1,-1):
-			print i," ",j
+	#		print i," ",j
                         lst.append(grid[i-j][j])
                 diag.append(lst)
         for j in xrange(0,row_size+1):
@@ -69,12 +66,40 @@ def print_grid(grid) :
 		print item #pretty print
 
 
-read_grid(sys.stdin,sys.stdout)
-print_grid(orig_grid)
-#vert_grid = alter_vert(orig_grid)
-print_grid(vert_grid)
-diag_left = alterDiagLeft(orig_grid)
-diag_right = alter_right(orig_grid)
-print "left grid\n",print_grid(diag_left)
-print "right grid\n",print_grid(diag_right)
-print "Script Done"
+def merge_characters(grid):
+	lst=[]
+	for item in grid:
+		lst.append(''.join(item))
+	return lst
+
+orig_grid=read_grid(sys.stdin,sys.stdout)
+#print_grid(orig_grid)
+vert_grid = alter_vert(orig_grid)
+#print_grid(vert_grid)
+left_grid = alter_left(orig_grid)
+right_grid = alter_right(orig_grid)
+print "left grid\n",print_grid(left_grid)
+print "right grid\n",print_grid(right_grid)
+def find_word(word):
+	global orig_grid
+	std_strs = merge_characters(orig_grid)
+	ver_strs = merge_characters(vert_grid)
+	left_strs = merge_characters(left_grid)
+	right_strs = merge_characters(right_grid)
+	for string in (std_strs + ver_strs + left_strs + right_strs):
+		result = word.find(string)
+		if result > -1:
+			return True
+	return False
+def find_words():
+	global WordList
+	lst=[]
+	for word in WordList:
+		lst.append(find_word(word))
+	return lst
+#print "Strings",print_grid(merge_characters(orig_grid))
+#print "Strings",print_grid(merge_characters(vert_grid))
+#print "Strings",print_grid(merge_characters(left_grid))
+#print "Strings",print_grid(merge_characters(right_grid))
+print "Results: ",print_grid(find_words())
+print "Done!"
